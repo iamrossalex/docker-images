f1b88c5bed68e57e9f6f172e715ef50e5c32fa8d
Ross Alex <wacdis.com@gmail.com>
2024-02-02 10:37:48 +0100

    up build

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd ./images/gulp
-docker build --no-cache -t wacdis/gulp .
+cd ./images/gulp-sync
+docker build --no-cache -t wacdis/gulp-sync .
 
 cd ../nginx-quic
 docker build --no-cache -t wacdis/nginx-quic .
@@ -11,6 +11,12 @@ docker build --no-cache -t wacdis/php:8.2 .
 
 cd ../php8.2-fpm
 docker build --no-cache -t wacdis/php-fpm:8.2 .
+
+cd ../php8.3
+docker build --no-cache -t wacdis/php:8.3 .
+
+cd ../php8.3-fpm
+docker build --no-cache -t wacdis/php-fpm:8.3 .
 
 docker pull mysql
 
